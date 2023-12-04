e1f1dbabbc4ffefe2a716d66cf83ddee5e417f75
Odysseus101 <kristjankgunnarsson@gmail.com>
2023-12-04 13:12:00 +0000

    add flight_route_logic

--- logic/flight_route_logic.py
+++ logic/flight_route_logic.py
@@ -1,5 +1,23 @@
 
 class Flight_route_logic():
 
-    def __init__ ():
+    def __init__ (self):
+        pass
+
+    def create_flight_route(self, data) -> None:
+        pass
+
+    def list_all_flight_routes(self) -> list: # FlightRoute
+        return
+
+    def list_flight_route(self, id): # FlightRoute
+        return
+    
+    def update_flight_route(self, id) -> None:
+        pass 
+
+    def delete_flight_route(self, id) -> None:
+        pass
+
+    def assign_pilot(pilot) -> None:
         pass
