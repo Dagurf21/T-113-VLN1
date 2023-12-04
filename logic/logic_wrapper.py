97defe6f7e64dab5ce3c8df09fba336eb1b3e820
Odysseus101 <kristjankgunnarsson@gmail.com>
2023-12-04 12:02:26 +0000

    add more functions in logic wrapper

--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -4,20 +4,68 @@ class Logic_Wrapper(object):
     def __init__(self):
         pass
 
-    def _employee_logic():
+    def _employee_logic(self):
         pass
 
-    def _flight_route_logic():
+    def _flight_route_logic(self):
         pass
 
-    def _destination_logic():
+    def _destination_logic(self):
         pass
 
-    def Voyage_logic():
+    def _Voyage_logic(self):
         pass
 
-    def plane_logic():
+    def _plane_logic(self):
         pass
+
+    def create_employee(self) -> None:
+        pass
+
+    def list_all_employees(self) -> list[]:
+        pass
+
+    def list_employee(self,id):
+        pass
+
+    def update_employee(self,id, data) -> None:
+        pass
+
+    def delete_employee(self,id) -> None:
+        pass
+
+    def create_flight_route(self, data) ->:
+        pass
+
+    def list_all_flight_routes(self) -> list:
+        pass
+
+    def list_flight_route(self, id):
+        pass
+
+    def update_flight_route(self, id) -> None:
+        pass
+
+    def delete_flight_route(self, id): void
++ assign_pilot(pilot): void
++ create_voyage(data): void
++ list_all_voyages(): list[Voyage..*]
++ list_voyage(id): Destination
++ update_voyage(id): void
++ delete_voyage(iid): void
++ create_destination(data): void
++ list_all_destinations(): list[Destination..*]
++ list_destination(id): Destination
++ update_destination(id): void
++ delete_destination(iid): void
++ create_plane(data): void
++ list_all_planes(): list[Plane...*]
++ list_plane(id): Plane
++ update_plane(id): void
++ delete_plane(iid): void
+
+
+
 
 
 
