3b403f5e1f7b061ec8164f694bee6aef669ab903
Marteinn23 <marteinn23@ru.is>
2023-12-04 13:01:21 +0000

    Changed the return hints a bit, for now

--- logic/destination_logic.py
+++ logic/destination_logic.py
@@ -5,10 +5,10 @@ class destination_logic:
     def create_destination(self, destination):
         self.destination = destination
 
-    def list_all_destinations(self) -> list[Destination]:
+    def list_all_destinations(self) -> list: #Destination
         return self.data_wrapper.list_all_destinations()
 
-    def list_destinations(self, id) -> Destination:
+    def list_destinations(self, id): # Destination
         return self.data_wrapper.list_destination(id)
 
     def update_destination(self, id) -> None:
