431c0d5f92ee674f033bd0fa8127820cb31a27ef
Marteinn23 <marteinn23@ru.is>
2023-12-04 12:09:29 +0000

    Added a Employee_Logic class

--- logic/employee_logic.py
+++ logic/employee_logic.py
@@ -1,4 +1,15 @@
 
+class Employee_Logic:
+    def __init__(self, data_connection):
+        self.data_wrapper = data_connection
+
+    def create_employee(self, employee):
+        """Takes in a employee object and forwards it to the data layer"""
+
+        self.data_wrapper.create_employee(employee)
+
+    def list_all_employees(self):
+        return self.data_wrapper.get_all_employees()
 
 '''
 from data.customer_data import Customer_Data
