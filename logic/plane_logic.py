d45cfda5a8fd3966e007f62517e88e4004b0556b
Odysseus101 <kristjankgunnarsson@gmail.com>
2023-12-04 14:02:01 +0000

    add

--- logic/plane_logic.py
+++ logic/plane_logic.py
@@ -2,8 +2,10 @@
 from data.data_wrapper import Data_Wrapper
 
 class Plane_logic(Data_Wrapper):
+    ''' '''
 
     def __init__(self):
+        ''' '''
         
         self.id: int
         self.name: str
@@ -17,18 +19,27 @@ class Plane_logic(Data_Wrapper):
         pass
 
     def create_plane(self, data) -> None:
-        
+        ''' '''
+
         pass
 
     def list_all_planes(self) -> list: # Plane
-        Data_Wrapper.get_all_planes()
-        return []
+        ''' '''
+        all_planes = Data_Wrapper.get_all_planes()
+        return all_planes
     
     def list_plane(self, id): # Plane
+        ''' '''
+        plane = Data_Wrapper.get_plane(id)
+        
+        return plane
+
         return
     
     def update_plane(self, id) -> None:
+        ''' '''
         pass
     
     def delete_plane(self, id) -> None:
+        ''' '''
         pass
