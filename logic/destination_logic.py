c7779744155d1d37a6abe5a1d9226ee89a839827
Marteinn23 <marteinn23@ru.is>
2023-12-04 14:18:04 +0000

    Added comments to destination logic class

--- logic/destination_logic.py
+++ logic/destination_logic.py
@@ -2,20 +2,26 @@ import data.data_wrapper as data_wrapper
 import model.destination as destination
 
 class Destination_Logic:
+    """This class is the logic layer for the destination class"""
     def __init__(self, data_connection):
         self.data_wrapper = data_connection
 
     def create_destination(self, destination):
+        """Takes in a destination object and forwards it to the data layer"""
         self.data_wrapper.create_destination(destination)
 
     def list_all_destinations(self) -> list:  # List of Destinations
+        """Returns a list of all destinations"""
         return self.data_wrapper.list_all_destinations()
 
     def list_destinations(self, id):  # Destination
+        """Returns a destination object with the given id"""
         return self.data_wrapper.list_destination(id)
 
     def update_destination(self, id) -> None:
+        """Updates a destination object with the given id"""
         self.data_wrapper.update_destination(id)
 
     def delete_destination(self, id) -> None:
+        """Deletes a destination object with the given id"""
         self.data_wrapper.delete_destination(id)
