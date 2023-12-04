ab0c203b1f0e08eccdcdf8a56a88e9c985baafe4
Marteinn23 <marteinn23@ru.is>
2023-12-04 13:17:47 +0000

    Added variables to the Voyage_Logic

--- logic/voyage_logic.py
+++ logic/voyage_logic.py
@@ -1,5 +1,12 @@
 class Voyage_Logic:
     def __init__(self) -> None:
+        
+        id: int
+        sold_seats: int
+        plane: Plane
+        departure_flight: FlightRoute
+        arrival_flight: FlightRoute
+        date: datetime
         pass
 
     def create_voyage(self, data) -> None:
