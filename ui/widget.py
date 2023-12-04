7cf623b6fdde1ca59a91dd067a2d29cee4f01028
Axel Snaer <hab123321123@gmail.com>
2023-12-04 13:38:36 +0000

    Added _print_datalist and minor cleanup in widgets.py

--- ui/widget.py
+++ ui/widget.py
@@ -4,18 +4,36 @@ UI_WIDTH = 69
 class UIWidget:
     # TODO: Implement
     def _clear_screen(self):
-        pass
+        # TEMPORARY!!!!!
+        for _ in range(50): print()
     
     def _print_options_list(self, lst: [str], numbered: bool = False):
+        """Prints a list of options centered around the ui width. List can optionally be numbered"""
         largest_option = max(map(len, lst))
         padding_len = (UI_WIDTH - largest_option) // 2
         padding = " " * padding_len
 
         for i, option in enumerate(lst):
             if numbered:
-                print(f"{padding}{i+1:>2} {option}{padding}")
+                print(f"{padding}{i+1:>2} {option}")
             else:
-                print(f"{padding}{option}{padding}")
+                print(f"{padding}{option}")
+
+    def _print_datalist(self, headers: {str: int}, data: [[str]]):
+        """Prints a centered table of data with headers.
+        Header format: { "label": col_size }
+        Data format: [ row1, row2, row3 ]
+        """
+
+        # Print headers
+        header_labels = [header.ljust(headers[header]) for header in headers]
+        print(" | ".join(header_labels).center(UI_WIDTH))
+
+        # Print rows
+        header_sizes = [headers[header] for header in headers]
+        for row in data:
+            data_row = [elem.ljust(header_sizes[i]) for i, elem in enumerate(row)]
+            print(" | ".join(data_row).center(UI_WIDTH))
 
     def _print_header(self, message = None, add_extra_newline: bool = False):
         print("""
