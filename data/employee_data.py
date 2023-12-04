6f8361019f2949bf84b95e7235d2d884d8180fe9
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-04 12:28:22 +0000

    Create employee with incrementin g id

--- data/employee_data.py
+++ data/employee_data.py
@@ -7,6 +7,7 @@ class Employee_Data:
         #print(os.getcwd())
         self.file_name = "files/employees.csv"
 
+
     def read_all_employees(self):
         ret_list = []
         with open(self.file_name, newline='', encoding="utf-8") as csvfile:
@@ -15,9 +16,22 @@ class Employee_Data:
                 ret_list.append(Employee(row["name"], row["birth_year"]))
         return ret_list
 
+
     def create_employee(self, employee):
         with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
-            fieldnames = ["name", "birth_year"]
+            fieldnames = ["id", "name", "job_title", "password", "address", "ssn", "mobile_phone", "email", "home_phone"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({'name': employee.name, 'birth_year': employee.birth_year})
+            id = self.get_new_id()
+            writer.writerow({'id': id, 'name': employee.name, 'job_title': employee.job_title, 'password': employee.password, 'address': employee.address, 'ssn': employee.ssn, 'mobile_phone': employee.mobile_phone, 'email': employee.email, 'home_phone': employee.home_phone})
+    
+
+    def get_new_id(self) -> int:
+        """Returns the id for a new employee"""
+        id = 0
+        with open(self.file_name, newline='', encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                id += 1
+
+            return id
