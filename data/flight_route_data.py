1f31ca8299cb0ee080efed2d2274fe2e66ac6142
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-05 10:28:07 +0000

    create flight route

--- data/flight_route_data.py
+++ data/flight_route_data.py
@@ -1,4 +1,5 @@
 from model.flight_route import FlightRoute
+import csv
 
 class FlightRoute_Data:
     def __init__(self):
@@ -6,14 +7,19 @@ class FlightRoute_Data:
     
 
     def create_flight_route(self, flight_route):
-        pass
+        with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
+            fieldnames = ["id", "flight_nr", "departure", "destination", "departure_time", "arrival_time"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            id = self.get_new_id()
+            writer.writerow({'id': id, 'flight_nr': flight_route.flight_nr, 'departure': flight_route.departure, 'destination': flight_route.destination, 'departure_time': flight_route.departure_time, 'arrival_time': flight_route.arrival_time})
 
     
     def get_new_id(self) -> int:
         """Returns a new id"""
         pass
 
-    
+
     def get_flight_route(self, flight_route_id):
         pass
 
