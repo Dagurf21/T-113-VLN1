c24cba796e931a605a53380a71074fd6f611ee37
Marteinn23 <marteinn23@ru.is>
2023-12-05 12:17:32 +0000

    Added destination methods

--- logic/destination_logic.py
+++ logic/destination_logic.py
@@ -10,7 +10,7 @@ class DestinationLogic:
 
     def create_destination(self, destination):
         """Takes in a destination object and forwards it to the data layer"""
-        self.data_wrapper.create_destination(destination)
+        return self.data_wrapper.create_destination(destination)
 
     def list_all_destinations(self) -> list:  # List of Destinations
         """Returns a list of all destinations"""
@@ -27,3 +27,24 @@ class DestinationLogic:
     def delete_destination(self, id) -> None:
         """Deletes a destination object with the given id"""
         self.data_wrapper.delete_destination(id)
+
+    def make_destination_class(self, destination_data, id=None):
+        """Creates a destination object with the given data and returns it"""
+        (
+            destination_country,
+            destination_airport,
+            destination_distance,
+            destination_flight_time,
+            destination_representative,
+            destination_emergency_number,
+        ) = destination_data
+        destination = Destination(
+            id=id,
+            country=destination_country,
+            airport=destination_airport,
+            distance_km=destination_distance,
+            flight_time=destination_flight_time,
+            representative=destination_representative,
+            emergency_number=destination_emergency_number,
+        )
+        return destination
