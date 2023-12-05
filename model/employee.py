9b2fc02addb18b7d368dbbc3f2366bb1cb68e786
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-05 11:35:01 +0000

    Add job_title to model

--- model/employee.py
+++ model/employee.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 class Employee:
     id: int = None
     name: str
+    job_title: str
     password_hash: str
     address: str
     ssn: str
     mobile_phone: str
     email: str
     home_phone: str
-
