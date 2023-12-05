f76eb30725b5d5248551e997c95963d914a2a514
Dagur Freyr Guðbrandsson <dagur.freyr2820@gmail.com>
2023-12-05 21:58:53 +0000

    Dagbókar færsla, Display all plane og display plane.

--- ui/planes_ui.py
+++ ui/planes_ui.py
@@ -1,4 +1,4 @@
-from ui.widget import UIWidget
+from ui.widget import UIWidget, UICancelException
 from model.plane import Plane # Dont think we need this ??
 from model.employee import Employee
 from logic.logic_wrapper import LogicWrapper
@@ -19,18 +19,16 @@ class PlaneUI(UIWidget):
                 "Register plane", 
                 "Remove plane", 
                 "Back",
-            ], True)
+            ], numbered=True)
 
             option = input("Choose an option: ")
 
             match option:
                 case "1": # List Planes
+                    self.display_plane_list()
                     self._clear_screen()
-                    self._print_header(message="List all planes")
-                    self.list_planes()
-                    self._clear_screen()
-                    self._print_header(message="Completed Successfully") # Not sure, may change
-
+                    self._print_header(add_extra_newline=True)
+                    
                 case "2": # List a plane
                     self._clear_screen()
                     self._print_header(message="List a singular plane")
@@ -59,11 +57,59 @@ class PlaneUI(UIWidget):
                     self._clear_screen()
                     self._print_header(message="Unknown option", add_extra_newline=True)
 
-    def list_planes(self):
-        print ("We are listing all planes here")
+    def display_plane_list(self):
+        planes = self.logic_wrapper.list_all_planes()
+        planes_data = []
 
-    def list_plane(self):
-        print ("We are printing a singular plane here")
+        for plane in planes:
+            planes_data.append([
+                plane.id,
+                plane.name, 
+                plane.type,
+                plane.manufacturer,
+                plane.capacity,
+                plane.flight
+            ])
+
+        self._prompt_interactive_datalist(
+            { "ID": 3, "name": 12, "Type": 4, "Availability": 12, "Destination": 14, "Flight ID": 10},
+            planes_data
+        )
+        # Should work, not sure about the spacing
+
+    def display_plane(self):
+        self._print_header("List plane", add_extra_newline=True)
+
+        while True:
+            try: 
+                plane_id = self._display_prompt("Enter plane id", opt_instruction="Leave empty to cancel", clear_screen=False)
+                plane_id = int(plane_id)
+
+                plane = self.logic_wrapper.list_plane(plane_id)
+
+                if plane == None:
+                    self._print_header("List Plane", add_extra_newline=True)
+                    self._print_centered(f"Plane with ID {plane_id} does not exist", add_newline_after=True)
+                    continue
+                
+                self._print_header(f"List Plane [id: {plane_id}]", add_extra_newline=True)
+                self._print_options_list([
+                    f"ID:           {plane.id}",
+                    f"Name:         {plane.name}",
+                    f"Type:         {plane.ty}",
+                    f"Manufacturer: {plane.manufacturer}",
+                    f"Capacity:     {plane.flights}"
+                ], add_newline_after=True)
+
+            
+            except ValueError:
+                self._print_header("List Plane", add_extra_newline=True)
+                self._print_centered("ID has to be a number", add_newline_after=True)
+                continue
+            except UICancelException:
+                return
+
+
 
     def register_plane(self):
         print ("We are registering a single plane here")
