4bf2e48307a33c91ce095c59f8dcfe190c7214a5
Marteinn23 <marteinn23@ru.is>
2023-12-05 15:08:07 +0000

    fixed type issue

--- model/voyage.py
+++ model/voyage.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
+from model.plane import Plane
+
 
 @dataclass(kw_only=True)
 class Voyage:
     id: int = None
     sold_seats: int
-    plane: int
+    plane: Plane
     departure_flight: int
     arrival_flight: int
     date: datetime
