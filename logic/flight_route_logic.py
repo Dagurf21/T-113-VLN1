59986d4ff17f9ded6dc95f1d0d42d6292b338bee
Odysseus101 <kristjankgunnarsson@gmail.com>
2023-12-05 13:26:08 +0000

    add __doc__ flight_route_logic

--- logic/flight_route_logic.py
+++ logic/flight_route_logic.py
@@ -3,11 +3,10 @@ from data.data_wrapper import DataWrapper
 from model.flight_route import FlightRoute
 
 class FlightRouteLogic():
-    ''' '''
+    '''This is the logic class for flight route'''
 
     def __init__ (self, data_wrapper):
-        ''' '''
-        pass
+        '''Initiates flightroutelogic through data_wrapper'''
 
     def create_flight_route(self, data: FlightRoute) -> None:
         '''Creates flight route:
@@ -21,23 +20,23 @@ class FlightRouteLogic():
         else:
             self.data_wrapper.create_flight_route(data)
 
-    def list_all_flight_routes(self) -> list: # FlightRoute
-        ''' '''
+    def list_all_flight_routes(self) -> list[FlightRoute]:
+        '''Gets list of all flight_routes from data_wrapper and forwards list of flight_routes'''
         return self.data_wrapper.get_all_flight_routes
     
     def list_flight_route(self, id): # FlightRoute
-        ''' '''
+        '''Gets a specific flight route with a specific ID'''
         return self.data_wrapper.get_flight_route(id)
     
     def update_flight_route(self, id) -> None:
-        ''' '''
+        '''Updates flightroute through data_wrapper'''
         self.data_wrapper.update_flight_route
 
     def delete_flight_route(self, id) -> None:
-        ''' '''
+        '''Deletes flight route through data_wrapper'''
         self.data_wrapper.delete_flight_route(id)
         
     def assign_pilot(self, pilot) -> None:
-        ''' '''
+        '''Assigns a pilot to a specific flight route'''
         self.data_wrapper
         pass
