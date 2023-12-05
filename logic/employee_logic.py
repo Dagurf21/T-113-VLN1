7123f35243dd820313f27faf53bec864bb09ab63
Marteinn23 <marteinn23@ru.is>
2023-12-05 12:23:29 +0000

    fixed logic wrapper issue

--- logic/employee_logic.py
+++ logic/employee_logic.py
@@ -14,9 +14,8 @@ class EmployeeLogic:
 
     def create_employee(self, employee_data):
         """Takes in a employee object and forwards it to the data layer"""
-        employee = self.make_employee_class(employee_data)
 
-        return self.data_wrapper.create_employee(employee)
+        return self.data_wrapper.create_employee(employee_data)
 
     def list_all_employees(self) -> list:
         """Returns a list of all employees"""
@@ -29,31 +28,15 @@ class EmployeeLogic:
         employee = self.data_wrapper.list_employee(id)
         return employee
 
-    def update_employee(self, id, data):
+    def update_employee(self, id, employee):
         """Updates a employee object with the given id"""
-        employee = self.make_employee_class(data, id)
+        employee.id = id
 
         return self.data_wrapper.update_employee(employee)
 
     def delete_employee(self, id):
         """Deletes a employee object with the given id"""
         return self.data_wrapper.delete_employee(id)
-
-    def make_employee_class(self, employee_data, employee_id=None):
-        """Creates a employee object with the given data and returns it"""
-        name, password, address, ssn, mobile_phone, email, home_phone = employee_data
-        employee = Manager(
-            id=employee_id,
-            name=name,
-            password_hash=password,
-            address=address,
-            ssn=ssn,
-            mobile_phone=mobile_phone,
-            email=email,
-            home_phone=home_phone,
-            work_phone="1234567",
-        )
-        return employee
 
 
 '''
