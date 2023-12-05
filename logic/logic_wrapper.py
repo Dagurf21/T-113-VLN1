1e6d222acefa0b0f3bfe35708b38cedcfc31f4e6
Marteinn23 <marteinn23@ru.is>
2023-12-05 09:38:02 +0000

    Formatted

--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -1,4 +1,4 @@
-#import os
+# import os
 from logic.employee_logic import EmployeeLogic
 from logic.flight_route_logic import FlightRouteLogic
 from logic.destination_logic import DestinationLogic
@@ -7,8 +7,8 @@ from logic.plane_logic import PlaneLogic
 
 from data.data_wrapper import DataWrapper
 
+
 class LogicWrapper(object):
-    
     def __init__(self):
         """Constructor for LogicWrapper"""
         self.data_wrapper = DataWrapper()
@@ -19,135 +19,127 @@ class LogicWrapper(object):
         self.plane_logic = PlaneLogic(self.data_wrapper)
 
     def _employee_logic(self):
-        ''' '''
-
+        """ """
 
     def _flight_route_logic(self):
-        ''' '''
+        """ """
         pass
 
     def _destination_logic(self):
-        ''' '''
+        """ """
         pass
 
     def _Voyage_logic(self):
-        ''' '''
+        """ """
         pass
 
     def _plane_logic(self):
-        ''' '''
+        """ """
         pass
 
     def create_employee(self) -> None:
-        ''' '''
+        """ """
         pass
 
-    def list_all_employees(self) -> list: # Employee
-        ''''''
+    def list_all_employees(self) -> list:  # Employee
+        """"""
         pass
 
-    def list_employee(self,id):
-        ''' '''
+    def list_employee(self, id):
+        """ """
         pass
 
-    def update_employee(self,id, data) -> None:
-        ''' '''
+    def update_employee(self, id, data) -> None:
+        """ """
         pass
 
-    def delete_employee(self,id) -> None:
-        ''' '''
+    def delete_employee(self, id) -> None:
+        """ """
         pass
 
     def create_flight_route(self, data) -> None:
-        ''' '''
+        """ """
         pass
 
-    def list_all_flight_routes(self) -> list: # Flight route
-        ''' '''
+    def list_all_flight_routes(self) -> list:  # Flight route
+        """ """
         return []
 
     def list_flight_route(self, id):
-        ''' '''
+        """ """
         pass
 
     def update_flight_route(self, id) -> None:
-        ''' '''
+        """ """
         pass
 
     def delete_flight_route(self, id) -> None:
-        ''' '''
+        """ """
         pass
 
     def assign_pilot(self, pilot) -> None:
-        ''' '''
+        """ """
         pass
 
     def create_voyage(self, data) -> None:
-        ''' '''
+        """ """
         pass
-    
+
     def list_all_voyages(self) -> list:
-        ''' '''
+        """ """
         return []
 
-    def list_voyage(self, id) -> list: # Destination
-        ''' '''
+    def list_voyage(self, id) -> list:  # Destination
+        """ """
         return
 
     def update_voyage(self, id) -> None:
-        ''' '''
+        """ """
         pass
 
-    def delete_voyage(self,id) -> None:
-        ''' '''
+    def delete_voyage(self, id) -> None:
+        """ """
         pass
 
     def create_destination(self, data) -> None:
-        ''' '''
+        """ """
         pass
-    
-    def list_all_destinations(self) -> list: # Destination
-        ''' '''
+
+    def list_all_destinations(self) -> list:  # Destination
+        """ """
         return []
 
-    def list_destination(self, id) -> list: # Destination
-        ''' '''
+    def list_destination(self, id) -> list:  # Destination
+        """ """
         return
 
     def update_destination(self, id) -> None:
-        ''' '''
+        """ """
         pass
 
     def delete_destination(self, id) -> None:
-        ''' '''
+        """ """
         pass
-    
+
     def create_plane(self, data) -> None:
-        ''' '''
+        """ """
         pass
-    
-    def list_all_planes(self) -> list: # Plane
-        ''' '''
+
+    def list_all_planes(self) -> list:  # Plane
+        """ """
         return []
 
-    def list_plane(self, id): # Plane
-        ''' '''
+    def list_plane(self, id):  # Plane
+        """ """
         return
 
     def update_plane(self, id) -> None:
-        ''' '''
+        """ """
         pass
 
     def delete_plane(self, id) -> None:
-        ''' '''
+        """ """
         pass
-
-
-
-
-
-
-
 
 
 '''
