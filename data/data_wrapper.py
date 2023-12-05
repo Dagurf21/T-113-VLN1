dc9d4fd6cfb61f07c4d5f3ff41342512cf0b4815
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-05 11:08:28 +0000

    Connecting wrapper to functions

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -1,8 +1,16 @@
 from data.employee_data import Employee_Data
+from data.destination_data import Destination_Data
+from data.flight_route_data import FlightRoute_Data
+from data.plane_data import Plane_Data
+from data.voyage_data import Voyage_Data
 
 class DataWrapper:
     def __init__(self):
         self.employee_data = Employee_Data()
+        self.destination_data = Destination_Data()
+        self.flight_route_data = FlightRoute_Data()
+        self.plane_data = Plane_Data()
+        self.voyage_data = Voyage_Data()
 
     def get_all_employees(self):
         return self.employee_data.read_all_employees()
@@ -11,70 +19,70 @@ class DataWrapper:
         return self.employee_data.create_employee(employee)
     
     def get_employee(self, employee_id):
-        pass
+        return self.employee_data.get_employee(employee_id)
 
-    def update_employee(self, employee_id):
-        pass
-
+    def update_employee(self, employee):
+        return self.employee_data.update_employee(employee)
+        
     def delete_employee(self, employee_id):
         pass
 
     def create_destination(self, destination):
-        pass
+        return self.destination_data.create_destination(destination)
 
     def get_all_destinations(self):
-        pass
+        return self.destination_data.get_all_destinations()
 
     def get_destination(self, destination_id):
-        pass
+        return self.destination_data.get_destination(destination_id)
 
-    def update_destination(self, destination_id):
-        pass
-
+    def update_destination(self, destination):
+        return self.destination_data.update_destination(destination)
+        
     def delete_destination(self, destination_id):
         pass
 
     def create_plane(self, plane):
-        pass
+        return self.plane_data.create_plane(plane)
 
     def get_all_planes(self):
-        pass
+        return self.plane_data.get_all_planes()
 
     def get_plane(self, plane_id):
-        pass
+        return self.plane_data.get_plane(plane_id)
 
-    def update_plane(self, plane_id):
-        pass
+    def update_plane(self, plane):
+        return self.plane_data.update_plane(plane)
 
     def delete_plane(self, plane_id):
         pass
 
     def create_voyage(self, voyage):
-        pass
+        return self.voyage_data.create_voyage(voyage)
 
     def get_all_voyages(self):
-        pass
+        return self.voyage_data.get_all_voyages()
 
     def get_voyage(self, voyage_id):
-        pass
+        return self.voyage_data.get_voyage(voyage_id)
 
-    def update_voyage(self, voyage_id):
-        pass
+    def update_voyage(self, voyage):
+        return self.voyage_data.update_voyage(voyage)
 
     def delete_voyage(self, voyage_id):
         pass
 
-    def create_flight(self, flight):
-        pass
+    def create_flight_route(self, flight_route):
+        return self.flight_route_data.create_flight_route(flight_route)
 
-    def get_all_flights(self):
-        pass
+    def get_all_flight_routes(self):
+        return self.flight_route_data.get_all_flight_routes()
     
-    def get_flight(self, flight_id):
-        pass
+    def get_flight_route(self, flight_route_id):
+        return self.flight_route_data.get_flight_route(flight_route_id)
 
-    def update_flight(self, flight_id):
-        pass
+    def update_flight_route(self, flight_route):
+        return self.flight_route_data.update_flight_route(flight_route)
 
     def delete_flight(self, flight_id):
         pass
