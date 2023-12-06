08bf405773e2001fa1a777be80d3f2456f114931
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-06 12:57:24 +0000

    Renaming flight_route to Flight

--- data/flight_data.py
+++ data/flight_data.py
@@ -1,15 +1,15 @@
-from model.flight_route import FlightRoute
+from model.flight import Flight
 import csv
 from tempfile import NamedTemporaryFile
 import shutil
 
-class FlightRouteData:
+class FlighData:
     def __init__(self):
-        self.file_name = "files/flight_routes.csv"
+        self.file_name = "files/flights.csv"
     
 
-    def create_flight_route(self, flight_route):
-        """Writes a new flight route to the storage file"""
+    def create_flight(self, flight):
+        """Writes a new flight to the storage file"""
         with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
             fieldnames = ["id", "flight_nr", "departure", "destination", "departure_time", "arrival_time"]
             
@@ -17,7 +17,7 @@ class FlightRouteData:
 
             id = self.get_new_id()
             
-            writer.writerow({'id': id, 'flight_nr': flight_route.flight_nr, 'departure': flight_route.departure, 'destination': flight_route.destination, 'departure_time': flight_route.departure_time, 'arrival_time': flight_route.arrival_time})
+            writer.writerow({'id': id, 'flight_nr': flight.flight_nr, 'departure': flight.departure, 'destination': flight.destination, 'departure_time': flight.departure_time, 'arrival_time': flight.arrival_time})
 
     
     def get_new_id(self) -> int:
@@ -32,50 +32,51 @@ class FlightRouteData:
             return id
 
 
-    def get_flight_route(self, flight_route_id):
-        """Returns the requested flight_route, if flight_route is not found returns None"""
+    def get_flight(self, flight_id):
+        """Returns the requested flight, if flight is not found returns None"""
         with open(self.file_name, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             
             for row in reader:
                 
-                if int(row["id"]) == flight_route_id:
-                    return FlightRoute(row["id"], row["flight_nr"], row["departure"], row["destination"], row["departure_time"], row["arrival_time"])
+                if int(row["id"]) == flight_id:
+                    return Flight(row["id"], row["flight_nr"], row["departure"], row["destination"], row["departure_time"], row["arrival_time"])
 
-            # If no flight_route is found with the given id, return None
+            # If no flight is found with the given id, return None
             return None
 
 
-    def get_all_flight_routes(self):
-        """Returns a list of all flight_routes"""
+    def get_all_flights(self):
+        """Returns a list of all flights"""
         ret_list = []
         
         with open(self.file_name, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             
             for row in reader:
-                ret_list.append(FlightRoute(row["id"], row["flight_nr"], row["departure"], row["destination"], row["departure_time"], row["arrival_time"]))
+                ret_list.append(Flight(row["id"], row["flight_nr"], row["departure"], row["destination"], row["departure_time"], row["arrival_time"]))
         
         return ret_list
 
 
-    def update_flight_route(self, flight_route):
-        """Updates the flight_route with the given id"""
+    def update_flight(self, flight):
+        """Updates the flight with the given id"""
         # Makes temporary file to not overwrite the original file
         tempfile = NamedTemporaryFile(mode='w', delete=False)
         
         with open(self.file_name, 'r', newline='', encoding="utf-8") as csvfile, tempfile:
             fieldnames = ["id", "flight_nr", "departure", "destination", "departure_time", "arrival_time"]
             
-            reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            reader = csv.DictReader(csvfile)
             writer = csv.DictWriter(tempfile, fieldnames=fieldnames)
 
+            writer.writeheader()
             for row in reader:
-                # Writes the flight_route with the new data into the temp file
-                if int(row["id"]) == flight_route.id:
-                    writer.writerow({'id': flight_route.id, 'flight_nr': flight_route.flight_nr, 'departure': flight_route.departure, 'destination': flight_route.destination, 'departure_time': flight_route.departure_time, 'arrival_time': flight_route.arrival_time})
+                # Writes the flight with the new data into the temp file
+                if int(row["id"]) == flight.id:
+                    writer.writerow({'id': flight.id, 'flight_nr': flight.flight_nr, 'departure': flight.departure, 'destination': flight.destination, 'departure_time': flight.departure_time, 'arrival_time': flight.arrival_time})
                 
-                # Writes the other flight_routes unchanged 
+                # Writes the other flights unchanged 
                 else:
                     writer.writerow({'id': row["id"], 'flight_nr': row["flight_nr"], 'departure': row["departure"], 'destination': row["destination"], 'departure_time': row["departure_time"], 'arrival_time': row["arrival_time"]})
 
@@ -83,7 +84,7 @@ class FlightRouteData:
         shutil.move(tempfile.name, self.file_name)
 
 
-    def delete_flight_route(self, flight_route_id):
+    def delete_flight(self, flight_id):
         pass
 
 
