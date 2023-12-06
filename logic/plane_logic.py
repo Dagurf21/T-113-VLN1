4b7a29e8d32b99591166b525e75b9aa9c2b8ddc5
Marteinn23 <marteinn23@ru.is>
2023-12-06 10:20:51 +0000

    Fixed plane_logic code, to my preferences(for now)

--- logic/plane_logic.py
+++ logic/plane_logic.py
@@ -1,45 +1,45 @@
-#import os
+# import os
 from data.data_wrapper import DataWrapper
 from model.plane import Plane
 
+
 class PlaneLogic:
-    '''This is a Logic class for Plane'''
+    """This is a Logic class for Plane"""
 
     def __init__(self, data_wrapper: DataWrapper):
-        '''Intitiatets plane with data_wrapper'''
+        """Intitiatets plane with data_wrapper"""
         self.data_wrapper = data_wrapper
 
-
     def create_plane(self, data) -> None:
-        '''Takes in plane data checks if ID is valid and forwards it to data layer'''
-        Plane = self.data_wrapper.plane
+        """Takes in plane data checks if ID is valid and forwards it to data layer"""
+        plane = data
 
-        if Plane.id == None:
+        if plane.id is None:
             self.data_wrapper.create_plane(data)
 
         else:
             raise ValueError("Invalid Input")
 
     def list_all_planes(self) -> list[Plane]:
-        '''Gets list of all planes through data wrapper and forwards list of planes'''
-        return self.data_wrapper.get_all_planesS
-    
+        """Gets list of all planes through data wrapper and forwards list of planes"""
+        return self.data_wrapper.get_all_planes
+
     def list_plane(self, id) -> Plane:
-        '''Gets info on a plane witha a specific ID and forwards data'''
+        """Gets info on a plane witha a specific ID and forwards data"""
         return self.data_wrapper.get_plane(id)
-    
+
     def update_plane(self, id, plane) -> None:
-        '''Updates information of a plane with a specific ID'''
-        
+        """Updates information of a plane with a specific ID"""
+
         plane.id = id
 
-        self.data_wrapper.update_plane(plane)
-    
-    def delete_plane(self, id, plane) -> None:
-        '''Removes a plane with specific ID'''
+        return self.data_wrapper.update_plane(plane)
 
-        if id == data_wrapper.get_plane.id:
-            self.data_wrapper.delete_plane
+    def delete_plane(self, id) -> None:
+        """Removes a plane with specific ID"""
+
+        if True:
+            return self.data_wrapper.delete_plane(id)
 
         else:
             raise ValueError("404 Invalid ID")
