e642dec6026718738780dbdf00adfe56dbe5a653
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-06 10:42:56 +0000

    Add get_employee_by_email & comments

--- data/employee_data.py
+++ data/employee_data.py
@@ -33,12 +33,13 @@ class EmployeeData:
 
 
     def create_employee(self, employee):
+        """Writes new employee into the storage file"""
         with open(self.file_name, 'a', encoding="utf-8") as csvfile:
             fieldnames = ["id", "name", "job_title", "license", "password", "address", "ssn", "mobile_phone", "email", "home_phone", "work_phone"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             id = self.get_new_id()
-            
+            # For assigning required variables to different employee types.
             if isinstance(employee, Manager):
                 job_title = "Manager"
                 pilot_license = None
@@ -59,6 +60,7 @@ class EmployeeData:
                 pilot_license = None
                 work_phone = employee.work_phone
 
+            # Writes the employee to the bottom of file
             writer.writerow({'id': id, 'name': employee.name, 'job_title': job_title, 'license': pilot_license, 'password': employee.password, 'address': employee.address, 'ssn': employee.ssn, 'mobile_phone': employee.mobile_phone, 'email': employee.email, 'home_phone': employee.home_phone, 'work_phone': work_phone})
             
     
@@ -75,20 +77,60 @@ class EmployeeData:
     
 
     def get_employee(self, employee_id):
+        """Returns the requested employee as the correct employee class type.
+        If no employee with the id is found return None
+        """
         with open(self.file_name, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
+
             for row in reader:
                 if int(row["id"]) == employee_id:
+
+                    # Finds out what employee subclass email is
                     if row["job_title"] == "Manager" or row["job_title"] == "Chuck Norris":
                         return Manager(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"], work_phone = row["work_phone"])
+                    
                     elif row["job_title"] == "Pilot":
                         return Pilot(id = int(row["id"]), name = row["name"], password = row["password"], license = row["license"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"])
+                    
                     elif row["job_title"] == "Flight Attendant":
                         return FlightAttendant(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"])
+                    
                     elif row["job_title"] == "Flight Manager":
                         return FlightManager(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"], work_phone = row["work_phone"])
+                    
                     else:
-                        pass
+                        return None
+
+            # If no employee is found with the given id, return None
+            return None
+        
+
+    def get_employee_by_email(self, employee_email):
+        """Returns the requested employee as the correct employee class type.
+        If no employee with the email is found return None
+        """
+        with open(self.file_name, newline='', encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                if row["email"] == employee_email:
+                    
+                    # Finds out what employee subclass email is
+                    if row["job_title"] == "Manager" or row["job_title"] == "Chuck Norris":
+                        return Manager(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"], work_phone = row["work_phone"])
+                    
+                    elif row["job_title"] == "Pilot":
+                        return Pilot(id = int(row["id"]), name = row["name"], password = row["password"], license = row["license"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"])
+                    
+                    elif row["job_title"] == "Flight Attendant":
+                        return FlightAttendant(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"])
+                    
+                    elif row["job_title"] == "Flight Manager":
+                        return FlightManager(id = int(row["id"]), name = row["name"], password = row["password"], address = row["address"], ssn = row["ssn"], mobile_phone = row["mobile_phone"], email = row["email"], home_phone = row["home_phone"], work_phone = row["work_phone"])
+                    
+                    else:
+                        return None
 
             # If no employee is found with the given id, return None
             return None
@@ -142,7 +184,7 @@ class EmployeeData:
                 # If the employee is found, the row is not written to the temporary file
                 if int(row["id"]) == employee_id:
                     row = {'id' : row["id"], 'name' : row["name"], 'job_title' : None, 'password' : None, 'address' : None, 'ssn' : None, 'mobile_phone' : None, 'email' : None, 'home_phone' : None, 'work_phone' : None}
-                
+
                 # Each row from the original file is written to the temporary file
                 else:
                     row = {'id': row["id"], 'name': row["name"], 'job_title': row["job_title"], 'license': row["license"], 'password': row["password"], 'address': row["address"], 'ssn': row["ssn"], 'mobile_phone': row["mobile_phone"], 'email': row["email"], 'home_phone': row["home_phone"], 'work_phone': row["work_phone"]}
