e32ecb94412b95f8c237fe96c95a8fe83c436c24
Axel Snaer <hab123321123@gmail.com>
2023-12-06 10:59:12 +0000

    Fix invalid options and flow error in main menu ui

--- ui/mainmenu_ui.py
+++ ui/mainmenu_ui.py
@@ -12,39 +12,40 @@ class MainMenuUI(UIWidget):
         self.logic_wrapper = logic_wrapper
 
     def show(self):
-        self._print_header(
-            message=f"Welcome {self.user.name}!",
-            add_extra_newline=True
-        )
+        while True:
+            self._print_header(
+                message=f"Welcome {self.user.name}!",
+                add_extra_newline=True
+            )
 
-        option = self._display_selection(
-            [
-                "Employees",
-                "Planes",
-                "Voyages",
-                "Destinations",
-                "Log out",
-            ],
-            include_back=False,
-        )
+            option = self._display_selection(
+                [
+                    "Employees",
+                    "Planes",
+                    "Voyages",
+                    "Destinations",
+                    "Log out",
+                ],
+                include_back=False,
+            )
 
-        match option:
-            case "1": # Employees
-                employee_ui = EmployeeUI(self.user, self.logic_wrapper)
-                employee_ui.show()
+            match option:
+                case 0: # Employees
+                    employee_ui = EmployeeUI(self.user, self.logic_wrapper)
+                    employee_ui.show()
 
-            case "2": # Planes
-                planes_ui = PlaneUI(self.user, self.logic_wrapper)
-                planes_ui.show()
+                case 1: # Planes
+                    planes_ui = PlaneUI(self.user, self.logic_wrapper)
+                    planes_ui.show()
 
-            case "3": # Voyage
-                voyage_ui = VoyageUI(self.user, self.logic_wrapper)
-                voyage_ui.show()
+                case 2: # Voyage
+                    voyage_ui = VoyageUI(self.user, self.logic_wrapper)
+                    voyage_ui.show()
 
-            case "4": # Planes
-                destination_ui = DestinationUI(self.user, self.logic_wrapper)
-                destination_ui.show()
-                
-            case "5": # Log out
-                return
+                case 3: # Planes
+                    destination_ui = DestinationUI(self.user, self.logic_wrapper)
+                    destination_ui.show()
+                    
+                case 4: # Log out
+                    return
 
