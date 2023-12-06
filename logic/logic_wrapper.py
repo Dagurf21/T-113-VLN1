6a00ca8d3301befdb62ef2285694737390427c15
Odysseus101 <kristjankgunnarsson@gmail.com>
2023-12-06 11:32:37 +0000

    Import for comments

--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -5,6 +5,16 @@ from logic.destination_logic import DestinationLogic
 from logic.voyage_logic import VoyageLogic
 from logic.plane_logic import PlaneLogic
 
+from model.destination import Destination
+from model.employee import Employee
+from model.flight_route import FlightRoute
+from model.flight_attendant import FlightAttendant
+from model.flight_manager import FlightManager
+from model.manager import Manager
+from model.pilot import Pilot
+from model.plane import Plane
+from model.voyage import Voyage
+
 from data.data_wrapper import DataWrapper
 
 
@@ -44,11 +54,11 @@ class LogicWrapper(object):
         """Creates an flight route"""
         return self.flight_route_logic.create_flight_route(data)
 
-    def list_all_flight_routes(self) -> list[flight_routes]:  # Flight route
+    def list_all_flight_routes(self) -> list[FlightRoute]:  # Flight route
         """Returns a list of all flight routes"""
         return self.flight_route_logic.list_all_flight_routes()
 
-    def list_flight_route(self, id) -> flight_route:  # Flight route
+    def list_flight_route(self, id) -> FlightRoute:  # Flight route
         """Returns a specific flight route/ via ID"""
         return self.flight_route_logic.list_flight_route(id)
 
@@ -72,7 +82,7 @@ class LogicWrapper(object):
         """Returns a list of all current voyages"""
         return self.voyage_logic.list_all_voyages()
 
-    def list_voyage(self, id) -> list[destination]:
+    def list_voyage(self, id) -> list[Destination]:
         """Returns a voyage/ via ID"""
         return self.voyage_logic.list_voyage(id)
 
@@ -88,11 +98,11 @@ class LogicWrapper(object):
         """Creates a new destination"""
         return self.destination_logic.create_destination(data)
 
-    def list_all_destinations(self) -> list[destination]:
+    def list_all_destinations(self) -> list[Destination]:
         """Returns a list of all destinations"""
         return self.destination_logic.list_all_destinations()
 
-    def list_destination(self, id) -> list[destination]:
+    def list_destination(self, id) -> list[Destination]:
         """Returns a Destination/ via ID"""
         return self.destination_logic.list_destination(id)
 
