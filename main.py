13beeae99b5c8d3e60fd828514c9cfdfa6711013
Axel Snaer <hab123321123@gmail.com>
2023-12-06 13:00:10 +0000

    Added catch for KeyboardInterrupt in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ui.login_ui import LoginUI
 from logic.logic_wrapper import LogicWrapper
 
-login_ui = LoginUI()
-login_ui.show()
+try:
+    login_ui = LoginUI()
+    login_ui.show()
+except KeyboardInterrupt:
+    print()
+    print("Stopping...")
 
