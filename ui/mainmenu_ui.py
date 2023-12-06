c81ee21d9db031b6e69a97f09c360eef0f23f66f
Axel Snaer <hab123321123@gmail.com>
2023-12-06 10:51:46 +0000

    Cleanup mainmenu_ui

--- ui/mainmenu_ui.py
+++ ui/mainmenu_ui.py
@@ -12,48 +12,39 @@ class MainMenuUI(UIWidget):
         self.logic_wrapper = logic_wrapper
 
     def show(self):
-        self._clear_screen()
-        self._print_header(message=f"Welcome {self.user.name}!", add_extra_newline=True)
+        self._print_header(
+            message=f"Welcome {self.user.name}!",
+            add_extra_newline=True
+        )
 
-        while True:
-            self._print_options_list([
+        option = self._display_selection(
+            [
                 "Employees",
                 "Planes",
                 "Voyages",
                 "Destinations",
                 "Log out",
-            ], True)
+            ],
+            include_back=False,
+        )
 
-            option = input("Choose an option: ")
+        match option:
+            case "1": # Employees
+                employee_ui = EmployeeUI(self.user, self.logic_wrapper)
+                employee_ui.show()
 
-            match option:
-                case "1": # Employees
-                    employee_ui = EmployeeUI(self.user, self.logic_wrapper)
-                    employee_ui.show()
-                    self._clear_screen()
-                    self._print_header(add_extra_newline=True)
+            case "2": # Planes
+                planes_ui = PlaneUI(self.user, self.logic_wrapper)
+                planes_ui.show()
 
-                case "2": # Planes
-                    planes_ui = PlaneUI(self.user, self.logic_wrapper)
-                    planes_ui.show()
-                    self._clear_screen()
-                    self._print_header(add_extra_newline=True)
+            case "3": # Voyage
+                voyage_ui = VoyageUI(self.user, self.logic_wrapper)
+                voyage_ui.show()
 
-                case "3": # Voyage
-                    voyage_ui = VoyageUI(self.user, self.logic_wrapper)
-                    voyage_ui.show()
-                    self._clear_screen()
-                    self._print_header(add_extra_newline=True)
-
-                case "4": # Planes
-                    destination_ui = DestinationUI(self.user, self.logic_wrapper)
-                    destination_ui.show()
-                    self._clear_screen()
-                    self._print_header(add_extra_newline=True)
-                    
-                case "5": # Log out
-                    break
+            case "4": # Planes
+                destination_ui = DestinationUI(self.user, self.logic_wrapper)
+                destination_ui.show()
                 
-                case _: # Unknown option, reprompt
-                    self._clear_screen()
-                    self._print_header(message="Unknown option", add_extra_newline=True)
+            case "5": # Log out
+                return
+
