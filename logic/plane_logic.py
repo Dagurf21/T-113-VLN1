75322170f2d10d2465157b1e766a65a808f27216
Marteinn23 <marteinn23@ru.is>
2023-12-08 13:48:35 +0000

    Added get plane method

--- logic/plane_logic.py
+++ logic/plane_logic.py
@@ -2,12 +2,13 @@
 from data.data_wrapper import DataWrapper
 from model.plane import Plane
 
+
 class PlaneLogic:
     """This is a Logic class for Plane"""
+
     def __init__(self, data_wrapper: DataWrapper) -> None:
         """Intitiatets plane with data_wrapper"""
         self.data_wrapper = data_wrapper
-
 
     def create_plane(self, plane) -> None:
         """Takes in plane data checks if ID is valid and forwards it to data layer"""
@@ -17,23 +18,24 @@ class PlaneLogic:
         else:
             raise ValueError("Invalid Input")
 
-
     def get_all_planes(self) -> list[Plane]:
         """Gets list of all planes through data wrapper and forwards list of planes"""
         return self.data_wrapper.get_all_planes()
 
-
-    def get_plane(self, id) -> Plane:
+    def get_plane(self, search_id) -> Plane:
         """Gets info on a plane witha a specific ID and forwards data"""
-        return self.data_wrapper.get_plane(id)
+        plane_list = self.get_all_planes()
 
+        for plane in plane_list:
+            if plane.id == search_id:
+                return plane
+        return None
 
     def update_plane(self, id, plane) -> None:
         """Updates information of a plane with a specific ID"""
         plane.id = id
 
         return self.data_wrapper.update_plane(plane)
-
 
     def delete_plane(self, id) -> None:
         """Removes a plane with specific ID"""
