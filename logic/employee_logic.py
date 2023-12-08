08504b5d126d0138618b1e53c7160fa9d9f6df73
Marteinn23 <marteinn23@ru.is>
2023-12-08 12:29:55 +0000

    Fixed get employee methods and fixed imports

--- logic/employee_logic.py
+++ logic/employee_logic.py
@@ -1,11 +1,5 @@
 from logic.logic_utilities import Validator
 
-from model.employee import Employee
-from model.pilot import Pilot
-from model.flight_attendant import FlightAttendant
-from model.manager import Manager
-from model.flight_manager import FlightManager
-
 VALIDATION_ERRORS = (
     "Invalid SSN. ",
     "Invalid Phone Number. ",
@@ -37,140 +31,28 @@ class EmployeeLogic:
 
         for employee in employee_list:
             if int(employee["id"]) == employee_id:
-                # Finds out what employee subclass email is
-                match employee["job_title"]:
-                    case "Manager" | "Chuck Norris":
-                        return Manager(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            work_phone=employee["work_phone"],
-                        )
+                return employee
+        return None
 
-                    case "Pilot":
-                        assignment_list = employee["assignments"].split(".")
-                        return Pilot(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            license=employee["license"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            assignments=assignment_list,
-                        )
-
-                    case "Flight Attendant":
-                        assignment_list = employee["assignments"].split(".")
-                        return FlightAttendant(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            assignments=assignment_list,
-                        )
-
-                    case "Flight Manager":
-                        return FlightManager(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            work_phone=employee["work_phone"],
-                        )
-
-                    case _:
-                        return None
-
-        pass
-
-    def get_employee_by_email(self, employee_email):
+    def get_employee_by_email(self, search_email):
         """Returns a employee object with the given id"""
-        employee_list = self.data_wrapper.get_employee_by_email(employee_email)
+        employee_list = self.list_all_employees()
 
         for employee in employee_list:
-            if employee["email"] == employee_email:
-                # Finds out what employee subclass the employee is
-                match employee["job_title"]:
-                    case "Manager" | "Chuck Norris":
-                        return Manager(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            work_phone=employee["work_phone"],
-                        )
+            if employee.email == search_email:
+                return employee
 
-                    case "Pilot":
-                        assignment_list = employee["assignments"].split(".")
-                        return Pilot(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            license=employee["license"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            assignments=assignment_list,
-                        )
-
-                    case "Flight Attendant":
-                        assignment_list = employee["assignments"].split(".")
-                        return FlightAttendant(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            assignments=assignment_list,
-                        )
-
-                    case "Flight Manager":
-                        return FlightManager(
-                            id=int(employee["id"]),
-                            name=employee["name"],
-                            password=employee["password"],
-                            address=employee["address"],
-                            ssn=employee["ssn"],
-                            mobile_phone=employee["mobile_phone"],
-                            email=employee["email"],
-                            home_phone=employee["home_phone"],
-                            work_phone=employee["work_phone"],
-                        )
-
-                    # If ID is found but employee has been deleted
-                    case _:
-                        return None
-
-    def get_employee_by_job(self, employee_email):
+    def get_employees_by_job(self, job_title):
         """Returns a employee object with the given id"""
-        employee = self.data_wrapper.get_employee_by_email(employee_email)
+        employee_list = self.list_all_employees()
 
-        pass
+        return_employee_list = []
+
+        for employee in employee_list:
+            if type(employee).__name__ == job_title:
+                return_employee_list.append(employee)
+
+        return employee_list
 
     def update_employee(self, employee):
         """Updates a employee object with the given id"""
