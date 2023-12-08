044a699945d7381c67cd05ec8f18fcfabebbd8fd
EliasHar <54663653+EliasHar@users.noreply.github.com>
2023-12-08 13:17:08 +0000

    Cleanup

--- logic/employee_logic.py
+++ logic/employee_logic.py
@@ -1,53 +1,47 @@
 from logic.logic_utilities import Validator
-
-VALIDATION_ERRORS = (
-    "Invalid SSN. ",
-    "Invalid Phone Number. ",
-    "Invalid Email. ",
-    "Invalid Home Number. ",
-)
-
+from model.employee import Employee
 
 class EmployeeLogic:
     """This class is the logic layer for the employee class"""
-    def __init__(self, data_connection):
+    def __init__(self, data_connection) -> None:
         self.data_wrapper = data_connection
         self.validate = Validator()
 
 
-    def create_employee(self, employee):
+    def create_employee(self, employee) -> None:
         """Takes in a employee object and forwards it to the data layer"""
         return self.data_wrapper.create_employee(employee)
 
 
-    def get_all_employees(self) -> list:
+    def get_all_employees(self) -> list["Employee"]:
         """Returns a list of all employees"""
-        employee_list = self.data_wrapper.get_all_employees()
-        return employee_list
+        return self.data_wrapper.get_all_employees()
 
 
-    def get_employee(self, employee_id):
+    def get_employee(self, employee_id) -> "Employee":
         """Returns the requested employee as the correct employee class type.
         If no employee with the id is found return None"""
         employee_list = self.list_all_employees()
 
         for employee in employee_list:
-            if int(employee["id"]) == employee_id:
+            if employee.id == employee_id:
                 return employee
         
         return None
 
 
-    def get_employee_by_email(self, search_email):
+    def get_employee_by_email(self, search_email) -> "Employee":
         """Returns a employee object with the given id"""
         employee_list = self.get_all_employees()
 
         for employee in employee_list:
             if employee.email == search_email:
                 return employee
+        
+        return None
 
 
-    def get_employees_by_job(self, job_title):
+    def get_employees_by_job(self, job_title) -> list["Employee"]:
         """Returns a employee object with the given id"""
         employee_list = self.get_all_employees()
 
@@ -57,18 +51,14 @@ class EmployeeLogic:
             if type(employee).__name__ == job_title:
                 employees_with_the_job.append(employee)
 
-        if employees_with_the_job is True:
-            return employee_list
-        
-        else:
-            return None
+        return employees_with_the_job
 
 
-    def update_employee(self, employee):
+    def update_employee(self, employee) -> None:
         """Updates a employee object with the given id"""
         return self.data_wrapper.update_employee(employee)
 
 
-    def delete_employee(self, id):
+    def delete_employee(self, id) -> None:
         """Deletes a employee object with the given id"""
         return self.data_wrapper.delete_employee(id)
--- logic/plane_logic.py
+++ logic/plane_logic.py
@@ -4,15 +4,13 @@ from model.plane import Plane
 
 class PlaneLogic:
     """This is a Logic class for Plane"""
-
-    def __init__(self, data_wrapper: DataWrapper):
+    def __init__(self, data_wrapper: DataWrapper) -> None:
         """Intitiatets plane with data_wrapper"""
         self.data_wrapper = data_wrapper
 
 
     def create_plane(self, plane) -> None:
         """Takes in plane data checks if ID is valid and forwards it to data layer"""
-        
         if plane.id is None:
             self.data_wrapper.create_plane(plane)
 
@@ -20,22 +18,18 @@ class PlaneLogic:
             raise ValueError("Invalid Input")
 
 
-    def list_all_planes(self) -> list[Plane]:
+    def get_all_planes(self) -> list[Plane]:
         """Gets list of all planes through data wrapper and forwards list of planes"""
-        
-        list_all_planes = self.data_wrapper.get_all_planes()
-
-        return list_all_planes 
+        return self.data_wrapper.get_all_planes()
 
 
-    def list_plane(self, id) -> Plane:
+    def get_plane(self, id) -> Plane:
         """Gets info on a plane witha a specific ID and forwards data"""
         return self.data_wrapper.get_plane(id)
 
 
     def update_plane(self, id, plane) -> None:
         """Updates information of a plane with a specific ID"""
-
         plane.id = id
 
         return self.data_wrapper.update_plane(plane)
@@ -43,9 +37,4 @@ class PlaneLogic:
 
     def delete_plane(self, id) -> None:
         """Removes a plane with specific ID"""
-
-        if True:
-            return self.data_wrapper.delete_plane(id)
-
-        else:
-            raise ValueError("404 Invalid ID")
+        return self.data_wrapper.delete_plane(id)
