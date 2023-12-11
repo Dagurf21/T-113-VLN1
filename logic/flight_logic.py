2e846f61220946b8dc8c5456b3decd7ce6997043
Marteinn23 <marteinn23@ru.is>
2023-12-11 09:30:43 +0000

    Implemented a get flights method

--- logic/flight_logic.py
+++ logic/flight_logic.py
@@ -14,46 +14,62 @@ class FlightLogic:
         self.data_wrapper = data_wrapper
         self.validator = Validator()
 
-
     def create_flight(self, departure, destination, data) -> str:
         """Creates flight, returns flight number"""
-        #self.validator.validate_destination(data)
+        # self.validator.validate_destination(data)
         flight_nr = self.create_flight_nr(destination)
         arrival_time = self.calculate_arrival_time(data.departure_time, destination)
-        self.data_wrapper.create_flight(Flight(flight_number = flight_nr, departure = departure, destination = destination, date = data.date, departure_time = data.departure_time, arrival_time = arrival_time))
+        self.data_wrapper.create_flight(
+            Flight(
+                flight_number=flight_nr,
+                departure=departure,
+                destination=destination,
+                date=data.date,
+                departure_time=data.departure_time,
+                arrival_time=arrival_time,
+            )
+        )
         return flight_nr
-    
 
     def create_flight_nr(self, destination_id) -> str:
         """Creates a flight number and returns it"""
 
-        flights_today = flights_within() # TODO put start of day and end of day into function
-        flights_today_to_destination = flighths_going_to(destination_id, flights_today) # maybe add ability to put flights of othere date
-
+        flights_today = (
+            flights_within()
+        )  # TODO put start of day and end of day into function
+        flights_today_to_destination = flights_going_to(
+            destination_id, flights_today
+        )  # maybe add ability to put flights of othere date
 
         flight_nr = f"NA0{destination_id}{nr_flight}"
         # TODO finna út seinustu töluna fyrir nr
 
-    def flights_going_to(self, destination, flights) -> list[Flight]: # With specific destination
-        '''Finds all flights with a specific destination'''
-        
-        flights_dest = [flight for flight in flights if flight.flight_nr[2:-1] == str(destination)]
+    def flights_going_to(
+        self, destination, flights
+    ) -> list[Flight]:  # With specific destination
+        """Finds all flights with a specific destination"""
+
+        flights_dest = [
+            flight for flight in flights if flight.flight_nr[2:-1] == str(destination)
+        ]
 
         return flights_dest
 
-    def flights_within(self,start_day,end_day) -> list[Flight]:
-        '''finds flights between start_day and end_day'''
-        
+    def flights_within(self, start_day, end_day) -> list[Flight]:
+        """finds flights between start_day and end_day"""
+
         all_flights = self.data_wrapper.get_all_destinations
-            
-        flights_within = [flight for flight in all_flights if day < end_day and day > start_day]
+
+        flights_within = [
+            flight for flight in all_flights if day < end_day and day > start_day
+        ]
 
         return flights_within
 
     def calculate_arrival_time(self, departure, destination_id) -> datetime:
         """Calculates the arrival time of a flight
         by adding the travel time from Destination to the departure time"""
-        
+
         detination = self.data_wrapper.get_destination(destination_id)
 
         arrival_time = departure.deltatime(destination.time)
@@ -61,7 +77,7 @@ class FlightLogic:
         return arrival_time
 
     def add_staff_to_flights():
-        '''Alows you to add staff to a flight after creating it'''
+        """Alows you to add staff to a flight after creating it"""
 
         # TODO Implement function
 
@@ -71,9 +87,14 @@ class FlightLogic:
         """Gets list of all flight from data_wrapper and forwards list of flight"""
         return self.data_wrapper.get_all_flight
 
-    def get_flight(self, id):  # Flight
+    def get_flight(self, flight_id):  # Flight
         """Gets a specific flight route with a specific ID"""
-        return self.data_wrapper.get_flight(id)
+        flight_list = self.get_all_flight
+
+        for flight in flight_list:
+            if flight.id == flight_id:
+                return flight
+        return None
 
     def update_flight(self, id, flight) -> None:
         """Updates flight through data_wrapper"""
