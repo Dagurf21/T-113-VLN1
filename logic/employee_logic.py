ee755055ec70e3a4e8ee468a87855270f9342b19
Marteinn23 <marteinn23@ru.is>
2023-12-11 09:04:57 +0000

    added password encoder to create employee

--- logic/employee_logic.py
+++ logic/employee_logic.py
@@ -1,22 +1,23 @@
-from logic import Validator
+from logic import Validator, Utilities
 from model import Employee
+
 
 class EmployeeLogic:
     """This class is the logic layer for the employee class"""
+
     def __init__(self, data_connection) -> None:
         self.data_wrapper = data_connection
         self.validate = Validator()
-
+        self.utility = Utilities()
 
     def create_employee(self, employee) -> None:
         """Takes in a employee object and forwards it to the data layer"""
+        employee.password = self.utility.password_encoder(employee.password)
         return self.data_wrapper.create_employee(employee)
-
 
     def get_all_employees(self) -> list["Employee"]:
         """Returns a list of all employees"""
         return self.data_wrapper.get_all_employees()
-
 
     def get_employee(self, employee_id) -> "Employee":
         """Returns the requested employee as the correct employee class type.
@@ -26,9 +27,8 @@ class EmployeeLogic:
         for employee in employee_list:
             if employee.id == employee_id:
                 return employee
-        
-        return None
 
+        return None
 
     def get_employee_by_email(self, search_email) -> "Employee":
         """Returns a employee object with the given id"""
@@ -37,9 +37,8 @@ class EmployeeLogic:
         for employee in employee_list:
             if employee.email == search_email:
                 return employee
-        
-        return None
 
+        return None
 
     def get_employees_by_job(self, job_title) -> list["Employee"]:
         """Returns a employee object with the given id"""
@@ -53,11 +52,9 @@ class EmployeeLogic:
 
         return employees_with_the_job
 
-
     def update_employee(self, employee) -> None:
         """Updates a employee object with the given id"""
         return self.data_wrapper.update_employee(employee)
-
 
     def delete_employee(self, id) -> None:
         """Deletes a employee object with the given id"""
