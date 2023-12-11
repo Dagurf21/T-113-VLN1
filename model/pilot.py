ff4956054c7e470c39d27b6e872e4293d053efa0
Marteinn23 <marteinn23@ru.is>
2023-12-11 11:08:22 +0000

    added lt to pilot class

--- model/pilot.py
+++ model/pilot.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
 from model import Employee
 
+
 @dataclass(kw_only=True)
 class Pilot(Employee):
     license: str
     assignments: list[int]
+
+    def __lt__(self, other_pilot):
+        return self.license < other_pilot.liscense
