23080e9bcf403eaf8f10ccb1416da626add5b263
Axel Snaer <hab123321123@gmail.com>
2023-12-13 15:06:14 +0000

    Merge bomb 3

--- ui/employee_ui.py
+++ ui/employee_ui.py
@@ -96,41 +96,71 @@ class EmployeeUI(UIElement):
             try:
                 option = self._display_selection(
                     [
-                        "ID",
-                        "Name",
-                        "License",
+                        "All",
+                        "With License",
                     ],
-                    header_title="List Pilots",
+                    header_title="List Pilots [Filter]",
                     include_back=True
                 )
             except UICancelException:
                 return
-
+            
             match option:
-                case "ID":
-                    employees.sort(key=lambda e: e.id)
-                case "Name":
-                    employees.sort(key=lambda e: e.name)
-                case "License":
-                    employees.sort(key=lambda e: e.license)
+                case "With License":
+                    try:
+                        license = self._prompt(
+                            "License",
+                            opt_instruction="Leave empty to cancel",
+                            header_title="List Pilots [Filter License]",
+                        )
+                    except UICancelException:
+                        continue
 
-            employee_data = []
+                    employees = [employee for employee in employees if employee.license == license]
+                case "Back":
+                    return
 
-            for employee in employees:
-                employee_data.append([
-                    employee.id,
-                    employee.name,
-                    employee.address,
-                    employee.mobile_phone,
-                    employee.license,
-                    employee.email
-                ])
+            while True:
+                try:
+                    option = self._display_selection(
+                        [
+                            "ID",
+                            "Name",
+                            "License",
+                        ],
+                        header_title="List Pilots [Sort]",
+                        include_back=True
+                    )
+                except UICancelException:
+                    return
 
-            self._display_interactive_datalist(
-                { "id": 3, "name": 8, "addr.": 10, "phone": 8, "license": 8, "email": 25 }, 
-                employee_data,
-                title="Employees",
-            )
+                match option:
+                    case "ID":
+                        employees.sort(key=lambda e: e.id)
+                    case "Name":
+                        employees.sort(key=lambda e: e.name)
+                    case "License":
+                        employees.sort(key=lambda e: e.license)
+                    case "Back":
+                        break
+
+                employee_data = []
+
+                for employee in employees:
+                    employee_data.append([
+                        employee.id,
+                        employee.name,
+                        employee.address,
+                        employee.mobile_phone,
+                        employee.license,
+                        employee.email
+                    ])
+
+                self._display_interactive_datalist(
+                    { "id": 3, "name": 8, "addr.": 10, "phone": 8, "license": 8, "email": 25 }, 
+                    employee_data,
+                    title="Employees",
+                )
 
     def list_all_flight_attendants(self):
         employees = self.logic_wrapper.get_employees_by_job("FlightAttendant")
--- ui/profile_ui.py
+++ ui/profile_ui.py
@@ -1,18 +1,43 @@
 from ui import UIElement, UICancelException
 from model import Employee, Pilot, Manager, FlightAttendant, FlightManager, Voyage, Destination, Flight
 from logic import LogicWrapper
+from colorama import Fore
 
-class Profile(UIElement):
+class ProfileUI(UIElement):
     def __init__(self, user: Employee, logic_wrapper: LogicWrapper):
         self.user = user
         self.logic_wrapper = logic_wrapper
 
     def show(self):
+        self._print_header(
+            message="My Profile",
+            add_extra_newline=True
+        )
+
         while True:
-            try:
-                pass
-            except UICancelException:
-                return
+            table = [
+                f"Id:      {self.user.id}",
+                f"Name:    {self.user.name}",
+                f"Email:   {self.user.email}",
+                f"SSN:     {self.user.ssn}",
+                f"Mobile:  {self.user.mobile_phone}",
+                f"Home:    {self.user.home_phone}"
+            ]
+
+            if isinstance(self.user, Manager):
+                table.append(f"Work:    {self.user.work_phone}")
+            if isinstance(self.user, FlightManager):
+                table.append(f"Work:    {self.user.work_phone}")
+            if isinstance(self.user, Pilot):
+                table.append(f"Assign:  {', '.join(self.user.assignments)}")
+                table.append(f"License: {self.user.license}")
+            if isinstance(self.user, FlightAttendant):
+                table.append(f"Assign:  {self.user.assignments}")
+
+            self._print_header(f"My Profile", add_extra_newline=True)
+            self._print_list(table, add_newline_after=True)
+
+            
             
     def get_voyages_in_this_week():
         pass
